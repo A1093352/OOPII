519b49ca2d4e9b4e6d16d4ddf467159d8104efb9
A1093352 <100329022+A1093352@users.noreply.github.com>
2022-09-29 20:36:35 +0800

    Create ticket.java

---
+++ Week3/ticket.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class ticketClass implements Runnable{
+	
+	static int tickets = 10000;
+	int pTickets;
+	Thread t;
+	
+	ticketClass(String machine){
+		pTickets = 0;
+		t = new Thread(this, machine);
+		t.start();
+	}
+	
+	public void run() {
+        Random rand = new Random();
+        int randNum = rand.nextInt(4)+1;
+		for(int i = 1 ; i <= randNum ; i++){
+            while(sellTickets()) {
+                pTickets++;
+            }
+        }
+		
+		System.out.println(t.getName()+"買到"+pTickets+"張高鐵票"); 
+	}
+	
+	private synchronized static boolean sellTickets(){
+		if(tickets > 0) {
+			tickets--;
+			return true;
+		}else
+			return false;
+	}
+	
+	
+}
+
+public class ticket {
+
+	public static void main(String[] args) {
+		ticketClass mA = new ticketClass("機器A");
+		ticketClass mB = new ticketClass("機器B");
+		ticketClass mC = new ticketClass("機器C");
+		ticketClass mD = new ticketClass("機器D");
+	}
+
+}
