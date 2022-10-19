bab5076d0b06dbade57c86fab1709addfe598259
A1093352 <100329022+A1093352@users.noreply.github.com>
2022-10-19 20:00:16 +0800

    Update Calculator.java

--- Week5/Calculator.java
+++ Week5/Calculator.java
@@ -1,5 +1,3 @@
-package OOPII;
-
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,26 +13,22 @@ class MyJFrame extends JFrame{
 	MyJFrame(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		setTitle("計算機");
-		setBounds(800, 200, 300, 450);
+		setBounds(800, 200, 300, 350);
 		setVisible(true);
 		
 		contentPane = new JPanel(new GridLayout(5,3,5,5));
         input = new JTextField(30);
-        input.setBorder(BorderFactory.createEmptyBorder(9, 9, 9, 9));
+        input.setPreferredSize(new Dimension(30,45));
+        input.setFont(new Font("Arial",Font.PLAIN,25));
         contentPane.add(input);
         
         String[] str = {"7","8","9","4","5","6","1","2","3","+","0","-","x","=","/"};
         JButton[] btn = new JButton[str.length];
         for(int i = 0 ; i < str.length ; i++) {
         	btn[i] = new JButton(str[i]);
-//        	btn[i].addActionListener(new ActionListener(){
-//        		public void actionPerformed(ActionEvent e) {
-//        			JOptionPane.showMessageDialog(null, btn[i].getText());
-//            	}
-//        	});
         	contentPane.add(btn[i]);
         }
-              
+        
         getContentPane().add(input, BorderLayout.NORTH);
         getContentPane().add(contentPane, BorderLayout.CENTER);
 
